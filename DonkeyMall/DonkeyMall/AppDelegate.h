eb1ad603688d2f4449bf0c0c265351bb90ebdc22
Donkey-Tao <taofei0610@gmail.com>
2016-10-12 19:21:52 +0800

    init

---
+++ DonkeyMall/DonkeyMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DonkeyMall
+//
+//  Created by 陶飞 on 16/10/12.
+//  Copyright © 2016年 TaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DonkeyMall/DonkeyMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DonkeyMall
+//
+//  Created by 陶飞 on 16/10/12.
+//  Copyright © 2016年 TaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
